4ba2160922c6468537eea3b5df216ec33c6c00d9
DicksonOndaraNyabochwa <yungondara@gmail.com>
2025-05-21 12:14:35 +0300

    did the code as per the errors

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, words):
+        matches = []
+        for candidate in words:
+            if candidate.lower() != self.word and sorted(candidate.lower()) == self.sorted_word:
+                matches.append(candidate)
+        return matches
